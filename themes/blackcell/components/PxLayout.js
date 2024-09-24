1eb5ecefe18701ea968e1f60873028dd17a4d346
bastKakrolot <blackcellcode@gmail.com>
2024-09-24 23:12:34 +0800

    fix：修改文章展示区域宽度

--- themes/blackcell/components/PxLayout.js
+++ themes/blackcell/components/PxLayout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-const containerWidth = 'px-7 max-w-7xl'
+const containerWidth = 'px-7 max-w-5xl'
 export default function PxLayout({children,className = ''}) {
   return (
     <div className={`${containerWidth} mx-auto ${className}`}>{children}</div>
